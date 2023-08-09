c14c4341532e31e40d4e3ec76e173cefc681ccf2
Fernando Almeida <nando.a.g.martines@gmail.com>
2023-08-09 18:24:05 -0300

    Simple Airbnb

---
+++ db/migrate/20230809205157_create_flats.rb
@@ -0,0 +1,13 @@
+class CreateFlats < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.integer :price_per_night
+      t.integer :number_of_guests
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
